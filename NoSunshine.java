f83fb8053ee61353a282f128a07b9b1eef2e4afd
printHelloWorld1000 <luca.drsampaio@gmail.com>
2026-09-23 11:48:24 -0700

    Create NoSunshine.java

---
+++ NoSunshine.java
@@ -0,0 +1,58 @@
+public class NoSunshine 
+{
+   public static void main(String[] args) 
+   {
+      
+      
+      
+      
+      System.out.print("And ");
+      iknow2();
+      iknow2();
+      
+      
+      jump();
+      jump();
+      System.out.println("Hey, I ought to leave young thing alone");
+      System.out.println("But ain't no sunshine when she's gone, whoa-whoa");
+      aintnosunshine();
+      System.out.println("Only darkness every day");
+      jump();
+      aintnosunshine();
+      andthishouse();
+      jump();
+      anytime();
+      anytime();
+      anytime();
+      anytime();
+   }
+   
+   public static void jump()
+   {
+      System.out.println("");
+   }
+   public static void anytime()
+   {
+      System.out.println("Anytime she goes away");
+   }
+   
+   public static void andthishouse()
+   {
+      System.out.println("And this house just ain't no home");
+   }
+   public static void aintnosunshine()
+   {
+      System.out.println("Ain't no sunshine when she's gone");
+   }
+   public static void iknow1()
+   {
+      System.out.println("I know, ");
+   }
+   public static void iknow2()
+   {
+      System.out.print("I know, I know, ");
+   
+   
+   
+   }
+}
